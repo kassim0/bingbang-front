6b4b0d10c3af8927c4febae1574a77fdccf4173c
kassim <kassim_haddad@yahoo.fr>
2026-08-27 22:42:43 +0200

    Mise en place de suppression de jeux mouahahaha

--- src/app/components/my-game-list/my-game-list.component.ts
+++ src/app/components/my-game-list/my-game-list.component.ts
@@ -1,13 +1,19 @@
 import {Component, Inject} from '@angular/core';
-import {GamesList} from "../../models/games.model";
-import {NgForOf} from "@angular/common";
+import {Game, GamesList} from "../../models/games.model";
+import {NgClass, NgForOf} from "@angular/common";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton, MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-my-game-list',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    MatIcon,
+    MatIconButton,
+    NgClass,
+    MatButton
   ],
   templateUrl: './my-game-list.component.html',
   styleUrl: './my-game-list.component.scss'
@@ -15,10 +21,27 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 export class MyGameListComponent {
 
   gamesList : GamesList;
+  selectedGames : Game[] = [];
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { gamesList: GamesList }) {
     this.gamesList = data.gamesList;
-    console.log(this.gamesList);
+  }
+
+  onDeleteGame(game: Game) {
+    if(this.isGameSelected(game)) {
+      this.selectedGames = this.selectedGames.filter(g=>g.id!==game.id);
+    }
+    else {
+      this.selectedGames.push(game);
+    }
+  }
+
+  isGameSelected(game:Game): boolean {
+    return this.selectedGames.some(g => g.id === game?.id);
+  }
+
+  saveModifGamesList(){
+
   }
 
 }
