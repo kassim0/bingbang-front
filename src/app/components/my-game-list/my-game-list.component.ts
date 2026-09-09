a151c72ce6283a103f20bf6fd0ddf10a228873ab
kassim <kassim_haddad@yahoo.fr>
2026-09-09 23:25:53 +0200

    Modification du nom de la gamesList

--- src/app/components/my-game-list/my-game-list.component.ts
+++ src/app/components/my-game-list/my-game-list.component.ts
@@ -5,6 +5,10 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {MatIcon} from "@angular/material/icon";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {GameApiService} from "../../services/game-api.service";
+import {FormsModule} from "@angular/forms";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {GamesSearchPopupComponent} from "../games-search-popup/games-search-popup.component";
 
 @Component({
   selector: 'app-my-game-list',
@@ -14,7 +18,10 @@ import {GameApiService} from "../../services/game-api.service";
     MatIcon,
     MatIconButton,
     NgClass,
-    MatButton
+    MatButton,
+    FormsModule,
+    MatFormField,
+    MatInput
   ],
   templateUrl: './my-game-list.component.html',
   styleUrl: './my-game-list.component.scss'
@@ -24,11 +31,13 @@ export class MyGameListComponent {
   gamesList : GamesList;
   selectedGames : Game[] = [];
   updateGamesList : UpdateGamesList;
+  editedName : string;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { gamesList: GamesList },
               private gameApiService: GameApiService,
               private dialogRef: MatDialogRef<MyGameListComponent>) {
     this.gamesList = data.gamesList;
+    this.editedName = data.gamesList.name;
     this.updateGamesList = {
       gamesListId: this.gamesList.id,
       newGameId: null,
@@ -52,6 +61,7 @@ export class MyGameListComponent {
 
   saveModifGamesList(){
     this.updateGamesList.removeGameId = this.selectedGames?.map(g => g.id);
+    this.updateGamesList.newName = this.editedName !== this.gamesList.name ? this.editedName : null;
     this.gameApiService.updateGamesList(this.updateGamesList).subscribe({
       next: res => {
         console.log('updateGamesList ok', res);
@@ -59,6 +69,20 @@ export class MyGameListComponent {
       },
       error: err => console.error('updateGamesList error', err),
     });
+  }
+
+  OpenGamesSearchPopup() {
+    const dialogRef = this.dialog.open(GamesSearchPopupComponent,{
+      width: '40%',
+      height:'90%',
+      data: {}
+    })
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.gameApiService.getGamesList().subscribe(gamesList => {
+        this.GamesLists = gamesList;
+      })
+    })
   }
 
 }
